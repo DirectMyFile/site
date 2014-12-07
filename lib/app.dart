5b8a839442001861f81bef7e22bbfbf803d98d2f
Michael Bullington <mikebullingtn@gmail.com>
2014-12-07 17:31:23 -0500

    Footer, header, the entire thing?
    - Moved window resize code into app.dart.
    - core-header-panel's waterfall-tall works correctly with the header again.
    - We have an actual footer now! Right now it has IRC contact information, as well as a link to our Github organization and GitLab server. Sticky-ness is still awful buggy. I have the same problem as this person. http://stackoverflow.com/questions/27279273/core-header-panel-and-sticky-footer
    - Clean up assorted markup.

--- lib/app.dart
+++ lib/app.dart
@@ -6,19 +6,22 @@ import 'package:core_elements/core_drawer_panel.dart';
 @CustomTag('dc-app')
 class DCAppElement extends PolymerElement {
 
-  int _previousWidth = 0;
-
+  int _preWidth = 0;
+  
   DCAppElement.created() : super.created() {
     $["header"].addEventListener('menu-toggle', (_) {
       toggleDrawer();
     });
   }
 
-  toggleDrawer() {
-    ($["drawer"] as CoreDrawerPanel).togglePanel();
-  }
+  toggleDrawer() => ($["drawer"] as CoreDrawerPanel).togglePanel();
 
-  closeDrawer() {
-    ($["drawer"] as CoreDrawerPanel).closeDrawer();
+  closeDrawer() => ($["drawer"] as CoreDrawerPanel).closeDrawer();
+
+  windowResize(int width) {
+    if(width >= 768 && width > _preWidth) {
+      (querySelector("dc-app") as DCAppElement).closeDrawer();
+    }
+    _preWidth = width;
   }
 }
--- web/main.dart
+++ web/main.dart
@@ -3,17 +3,11 @@ import 'dart:html';
 
 import 'package:site/app.dart';
 
-int _preWidth = 0;
-
 void main() {
   initPolymer().run(() {
     Polymer.onReady.then((_) {
       window.onResize.listen((_) {
-        if(window.innerWidth >= 768) {
-          if(window.innerWidth > _preWidth)
-            (querySelector("dc-app") as DCAppElement).closeDrawer();
-        }
-        _preWidth = window.innerWidth;
+        (querySelector("dc-app") as DCAppElement).windowResize(window.innerWidth);
       });
     });
   });
